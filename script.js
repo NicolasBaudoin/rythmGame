74f8b24d238758748576ea87cddbfd6b51ae0eb7
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2025-10-03 11:31:43 +0200

    Variable declaration for the game script

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameScreen');
+const ctx = canvas.getContext('2d');
+const startButton = document.querySelector('.btn.start');
+const stopButton = document.querySelector('.btn.stop');
+const scoreDisplay = document.getElementById('score');
+const music = document.getElementById('music');
